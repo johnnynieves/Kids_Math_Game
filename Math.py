651a67de64cf6f0ff1b9122a2040af009a4eb46a
johnnynieves <johnny.nieves@live.com>
2019-06-18 13:18:15 -0700

    addition

--- Math.py
+++ Math.py
@@ -1,26 +1,58 @@
 #Kids Math game because i am cheap
 #Need to add Level of Difficulty
 #Need to add limit level math (answer can not be greater than the max of level)
-#Need to add for loops where need 
+#Need to add for loops where need  X
 #Need to add operations
 #Need to add try except
 #First program lets see what happens
 
+
 import math
 import random
-a = random.randint(0, 10)
-b = random.randint(0, 10)
-ans = a + b
+
+
+
+
+i = 1
 print('***********************************************************')
 print('******************   NIEVES MATH GAME   *******************')
-print('*********************************************************** \n')
+print('***********************************************************')
+def addition():
+    pass
+for i in range(1, 11):
+    a = random.randint(0, 10)
+    b = random.randint(0, 10)
+    ans = a + b
+    print('*                                                         *')
+    print('*                                                         *')   
+    print("*   Please enter your answer                              *")
+    print('*                                                         *')
+    print('*                                                         *')
+    print("*     " + str(a) + "                                                   *")
+    print("*    +" + str(b) + "                                                   *")
+    print('* _________                                               *')
+    urans = input('*    ')
+    if urans == ans:
+        print('*                                                         *\n* GREAT JOB!!                                             *')
+        print('*                                                         *')
+        print('*                                                         *')
+        print('*                                                         *')
+    elif urans != ans:
+        print('*                                                         *\n* Oh Oh!! Try Again Please.                               *')
+        while urans != ans:
+            urans = input('*    ')
+            if urans == ans:
+                print('*                                                         *\n* GREAT JOB!!                                             *')
+                print('*                                                         *')
+            elif urans != ans:
+                print('*                                                         *\n* Oh Oh!! Try Again Please.                               *\n*')
+    i = i+1
+print('***********************************************************')
 
-print("Please enter your answer \n")
-print(" " + str(a))
-print("+" + str(b))
-print('_______')
-urans = input()
-if urans == ans:
-    print('GREAT JOB!!')
-elif urans != ans:
-    print('Oh Oh!! Try Again Please.')
+
+#cont = (input('* Do you want to keep Going?                              *\n')
+#    if cont == 'Y' or 'Yes' or 'y' or 'yes':
+#        i = 1
+#    elif cont == 'N' or 'No' or 'n' or 'no':
+print('*                  The Game is Over                        ')
+print('***********************************************************')  
